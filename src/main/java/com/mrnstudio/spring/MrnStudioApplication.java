c22e69107eb64eff535ded333afdac52eefec24b
DESKTOP-5AVO1VN\khpg2 <goroweon@gmail.com>
2022-05-20 10:00:48 +0900

    project 생성
    -intelliJ
    -java
    -spring
    -gradle

---
+++ src/main/java/com/mrnstudio/spring/MrnStudioApplication.java
@@ -0,0 +1,11 @@
+package com.mrnstudio.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrnStudioApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MrnStudioApplication.class, args);
+    }
+}
